71f9349a29164941c2c687f6cf706911b6ab4769
Junyong Park <jyp0802@hotmail.com>
2018-12-07 18:01:18 +0900

    prayer add function

--- routes.js
+++ routes.js
@@ -30,4 +30,15 @@ module.exports = function(app) {
 		res.render('ccc.ejs');
 	});
 
+	app.post('/addprayer', function(req, res){
+		var detail = req.body.pray_detail;
+		var user = req.body.pray_name;
+		connection.query("INSERT INTO Prayers (user, detail, post_date) values (?,?,NOW());", [user, detail], function(err, rows) {
+			if (err)
+				console.log(err);
+			else {
+				res.redirect('/prayer');
+			}
+		});
+	});
 };
