20f85734eb83cb8100ae339ea2fd6a59aaa8f95e
Junyong Park <jyp0802@hotmail.com>
2018-12-15 00:14:32 +0900

    connection reset

--- routes.js
+++ routes.js
@@ -4,6 +4,29 @@ var mysql = require('mysql');
 var dbconfig = require('./config/database');
 var connection = mysql.createConnection(dbconfig.connection);
 
+function handleDisconnect() {
+    console.log('handleDisconnect()');
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                    // the old one cannot be reused.
+    connection.connect(function(err) {              // The server is either down
+    if(err) {                                      // or restarting (takes a while sometimes).
+        console.log(' Error when connecting to db:', err);
+        setTimeout(handleDisconnect, 1000);         // We introduce a delay before attempting to reconnect,
+    }                                               // to avoid a hot loop, and to allow our node script to
+    });                                             // process asynchronous requests in the meantime.
+                                                    // If you're also serving http, display a 503 error.
+
+    connection.on('  Database Error', function(err) {
+        console.log('db error: ' + err.code, err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                       // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                // server variable configures this)
+        }
+    });
+
+}
+
 connection.query('USE ' + dbconfig.database);
 
 module.exports = function(app) {
@@ -17,6 +40,10 @@ module.exports = function(app) {
 	});
 
 	app.get('/prayer', function(req, res) {
+		connection.connect(function(err) {
+            if(err)
+                setTimeout(handleDisconnect, 1000);
+		});
 		connection.query("SELECT * FROM MyPrayers", function(err1, row1) {
 			if (err1)
 				console.log(err1);
@@ -33,6 +60,10 @@ module.exports = function(app) {
 	});
 
 	app.get('/prayer_edit', function(req, res) {
+		connection.connect(function(err) {
+            if(err)
+                setTimeout(handleDisconnect, 1000);
+		});
 		connection.query("SELECT * FROM MyPrayers", function(err1, row1) {
 			if (err1)
 				console.log(err1);
@@ -53,6 +84,10 @@ module.exports = function(app) {
 	});
 
 	app.post('/addotherprayer', function(req, res){
+		connection.connect(function(err) {
+            if(err)
+                setTimeout(handleDisconnect, 1000);
+		});
 		var detail = req.body.pray_detail;
 		var user = req.body.pray_name;
 		if (user == "")
@@ -67,6 +102,10 @@ module.exports = function(app) {
 	});
 	
 	app.post('/addmyprayer', function(req, res){
+		connection.connect(function(err) {
+            if(err)
+                setTimeout(handleDisconnect, 1000);
+		});
 		var detail = req.body.pray_add;
 		connection.query("INSERT INTO MyPrayers (detail) values (?);", [detail], function(err, rows) {
 			if (err)
